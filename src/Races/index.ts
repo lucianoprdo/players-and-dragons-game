b1a8c3870e4d6f899be0090db4aeb35244a78a9d
Luciano Prado <lucianobatuta@live.com>
2024-05-10 09:29:26 -0300

    Create the Halfling and Orc class

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,5 +1,7 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfling';
+import Orc from './Orc';
 
-export default { Race, Dwarf, Elf };
+export default { Race, Dwarf, Elf, Halfling, Orc };
